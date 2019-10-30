b6ccd536717b6656d9a71619b0156e562b671427
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-10-30 11:17:11 +0000

    Snapshot of branch master taken on 2019-10-30T07:17:06.931-04:00 from "https://github.com/UnofficialJuliaMirror/EAGO.jl-bb8be931-2a91-5aca-9f87-79e1cb69959a"

--- src/eago_semiinfinite/semi_infinite.jl
+++ src/eago_semiinfinite/semi_infinite.jl
@@ -1,12 +1,13 @@
   mutable struct SIPResult
-      iteration_number::Int
+      iteration_number::Int64
       upper_bound::Float64
       lower_bound::Float64
       feasibility::Bool
       xsol::Vector{Float64}
+      psol::Vector{Float64}
       solution_time::Float64
   end
-  SIPResult() = SIPResult(1, Inf, -Inf, true, Float64[], 0.0)
+  SIPResult() = SIPResult(1, Inf, -Inf, true, Float64[], Float64[], 0.0)
 
   """
       SIP_Problem_Storage
@@ -19,53 +20,70 @@ mutable struct SIPProblem
     p_l::Vector{Float64}
     p_u::Vector{Float64}
 
-    np::Int
-    nSIP::Int
-    nx::Int
+    np::Int64
+    nSIP::Int64
+    nx::Int64
     sense::Symbol
 
     init_lower_disc::Vector{Vector{Vector{Float64}}}
     init_upper_disc::Vector{Vector{Vector{Float64}}}
 
     absolute_tolerance::Float64
-    iteration_limit::Int
+    constraint_tolerance::Float64
+    iteration_limit::Int64
     initial_eps_g::Float64
     initial_r::Float64
 
     return_hist::Bool
-    header_interval::Int
-    print_interval::Int
-    verbosity::Int
+    header_interval::Int64
+    print_interval::Int64
+    verbosity::Int64
+
+    optimizer
+    kwargs
   end
-  function SIPProblem(x_l::Vector{Float64}, x_u::Vector{Float64}, p_l::Vector{Float64}, p_u::Vector{Float64}, kwargs)
+  function SIPProblem(x_l::Vector{Float64}, x_u::Vector{Float64},
+                      p_l::Vector{Float64}, p_u::Vector{Float64},
+                      gSIP, optimizer, kwargs)
 
       np = length(p_l)
       nx = length(x_l)
 
-      absolute_tolerance = haskey(kwargs, :absolute_tolerance) ? kwargs[:return_hist] : 1E-1
-      iteration_limit = haskey(kwargs, :iteration_limit) ? kwargs[:return_hist] : 1000
-      initial_eps_g = haskey(kwargs, :initial_eps_g) ? kwargs[:return_hist] : 0.9
-      initial_r = haskey(kwargs, :initial_r) ? kwargs[:return_hist] : 1.5
+      absolute_tolerance = haskey(kwargs, :sip_absolute_tolerance) ? kwargs[:sip_absolute_tolerance] : 1E-3
+      constraint_tolerance = haskey(kwargs, :sip_constraint_tolerance) ? kwargs[:sip_constraint_tolerance] : 1E-3
+      iteration_limit = haskey(kwargs, :sip_iteration_limit) ? kwargs[:sip_iteration_limit] : 100
+      initial_eps_g = haskey(kwargs, :sip_initial_eps_g) ? kwargs[:sip_initial_eps_g] : 1.0
+      initial_r = haskey(kwargs, :sip_initial_r) ? kwargs[:sip_initial_r] : 2.0
 
-      return_hist = haskey(kwargs, :return_hist) ? kwargs[:return_hist] : false
-      header_interval = haskey(kwargs, :header_interval) ? kwargs[:header_interval] : 20
-      print_interval = haskey(kwargs, :print_interval) ? kwargs[:print_interval] : 1
-      verbosity = haskey(kwargs, :verbosity) ? kwargs[:verbosity] : 1
+      return_hist = haskey(kwargs, :sip_return_hist) ? kwargs[:sip_return_hist] : false
+      header_interval = haskey(kwargs, :sip_header_interval) ? kwargs[:sip_header_interval] : 20
+      print_interval = haskey(kwargs, :sip_print_interval) ? kwargs[:sip_print_interval] : 1
+      verbosity = haskey(kwargs, :sip_verbosity) ? kwargs[:sip_verbosity] : 1
 
-      sense = haskey(kwargs, :sense) ? kwargs[:sense] : :min
-      init_lower_disc = haskey(kwargs, :init_lower_disc) ? kwargs[:init_lower_disc] : Vector{Vector{Float64}}[]
-      init_upper_disc = haskey(kwargs, :init_upper_disc) ? kwargs[:init_upper_disc] : Vector{Vector{Float64}}[]
+      sense = haskey(kwargs, :sip_sense) ? kwargs[:sip_sense] : :min
+      init_lower_disc = haskey(kwargs, :sip_init_lower_disc) ? kwargs[:sip_init_lower_disc] : Vector{Vector{Float64}}[]
+      init_upper_disc = haskey(kwargs, :sip_init_upper_disc) ? kwargs[:sip_init_upper_disc] : Vector{Vector{Float64}}[]
 
-      nSIP = count(x -> String(x)[1:4] === "gSIP", keys(kwargs))
+      opt_dict = Dict{Symbol,Any}()
+      for key in keys(kwargs)
+        string_key = String(key)
+        if string_key[1:3] !== "sip"
+          opt_dict[key] = kwargs[key]
+        end
+      end
+
+      nSIP = length(gSIP)
 
       SIPProblem(x_l, x_u, p_l, p_u, np, nSIP, nx, sense, init_lower_disc,
-                 init_upper_disc, absolute_tolerance, iteration_limit,
+                 init_upper_disc, absolute_tolerance, constraint_tolerance,
+                 iteration_limit,
                  initial_eps_g, initial_r, return_hist, header_interval,
-                 print_interval, verbosity)
+                 print_interval, verbosity, optimizer, opt_dict)
   end
 
-  function print_int!(verbosity::Int, hdr_intv::Int, prt_intv::Int, k_int::Int,
-                      lbd::Float64, ubd::Float64, eps::Float64, r::Float64)
+  function print_int!(verbosity::Int64, hdr_intv::Int64, prt_intv::Int64,
+                      k_int::Int64, lbd::Float64, ubd::Float64,
+                      eps::Float64, r::Float64, ismin::Bool)
 
     if (verbosity == 1 || verbosity == 2)
 
@@ -79,32 +97,39 @@ mutable struct SIPProblem
 
         print_str = "| "
 
-        max_len = 9
+        max_len = 15
         temp_str = string(k_int)
         len_str = length(temp_str)
         print_str *= (" "^(max_len - len_str))*temp_str*" | "
 
-        max_len = 11
-        temp_str = string(round(lbd, digits = 6))
+        max_len = 15
+        lower_adj = ismin ? lbd : ubd
+        temp_str = string(round(lower_adj, digits = 6))
         len_str = length(temp_str)
         print_str *= (" "^(max_len - len_str))*temp_str*" | "
 
-        max_len = 11
-        temp_str = string(round(ubd, digits = 6))
+        max_len = 15
+        upper_adj = ismin ? ubd : lbd
+        temp_str = string(round(upper_adj, digits = 6))
         len_str = length(temp_str)
         print_str *= (" "^(max_len - len_str))*temp_str*" | "
 
-        max_len = 7
+        max_len = 15
         temp_str = string(round(eps, digits = 6))
         len_str = length(temp_str)
         print_str *= (" "^(max_len - len_str))*temp_str*" | "
 
-        max_len = 5
+        max_len =15
+        temp_str = string(round(r, digits = 5))
+        len_str = length(temp_str)
+        print_str *= (" "^(max_len - len_str))*temp_str*" | "
+
+        max_len = 15
         temp_str = string(round((ubd-lbd), digits = 5))
         len_str = length(temp_str)
         print_str *= (" "^(max_len - len_str))*temp_str*" | "
 
-        max_len = 7
+        max_len = 15
         temp_str = string(round((ubd-lbd)/abs(ubd), digits = 6))
         len_str = length(temp_str)
         print_str *= (" "^(max_len - len_str))*temp_str*" |"
@@ -115,7 +140,8 @@ mutable struct SIPProblem
     return
   end
 
-  function print_summary!(verb::Int, val::Float64, x::Vector{Float64}, feas::Bool, desc::String)
+  function print_summary!(verb::Int64, val::Float64, x::Vector{Float64},
+                          feas::Bool, desc::String)
     (verb == 2 || verb == 1) && println("solved $desc: ", val, " ", x, " ", feas)
     return
   end
@@ -126,7 +152,7 @@ mutable struct SIPProblem
     return
   end
 
-  function check_convergence(LBD::Float64, UBD::Float64, atol::Float64)
+  function check_convergence(LBD::Float64, UBD::Float64, atol::Float64, verb::Int64)
     if (abs(UBD - LBD) < atol)
       (verb == 2 || verb == 1) && println("Algorithm Converged")
       return true
@@ -137,7 +163,7 @@ mutable struct SIPProblem
   function llp2_update(llp2_obj_val::Float64, r::Float64, eps_g::Float64,
                        xbar::Vector{Float64}, pbar::Vector{Float64},
                        UBD::Float64, UBDg::Float64,
-                       disc_set::Vector{Vector{Vector{Float64}}}, i::Int)
+                       disc_set::Vector{Vector{Vector{Float64}}}, i::Int64)
     xstar = fill(NaN,(length(xbar),))
     UBDtemp = UBDg
     eps_g_out = eps_g
@@ -151,6 +177,11 @@ mutable struct SIPProblem
       push!(disc_set[i], pbar)
     end
     return UBDtemp, xstar, eps_g_out
+  end
+
+  struct SIPCallback
+    f
+    gSIP
   end
 
   include("sip_explicit.jl")
